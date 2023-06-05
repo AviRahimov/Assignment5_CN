4d23f15ebecd34305d315a68f52e536a9bbdb87e
Avi Rahimov <rahimovavi3@gmail.com>
2023-06-05 16:06:38 +0300

    update sniffer, need to fix the 5 last flags

--- sniffer.py
+++ sniffer.py
@@ -8,66 +8,60 @@ def process_tcp_packet(packet):
     This function as her name says, process TCP packets.
     We extract some information (source_ip, destination_ip...).
     """
-    source_ip = packet[IP].src
-    destination_ip = packet[IP].dst
-    source_port = packet[TCP].sport
-    destination_port = packet[TCP].dport
-    timestamp = packet.time
-    total_length = len(packet)
-    cache_flag = packet.getlayer(Raw).load
+    processing_packet(packet)
 
-
-    # write the extracted data to the output file
-    with open("output.txt", "a") as file:
-        file.write(f"{{ source_ip: {source_ip}, dest_ip: {destination_ip}, source_port: {source_port}, dest_port: {destination_port}, protocol: TCP, timestamp: {timestamp}, total_length: {total_length}, cache_flag: {cache_flag.hex()}, data: {packet.payload.hex()} }}\n")
 
 def process_udp_packet(packet):
     """
     This function as her name says, process UDP packets.
     Overall, this function do as the function above but for UDP protocol
     """
-    source_ip = packet[IP].src
-    destination_ip = packet[IP].dst
-    source_port = packet[UDP].sport
-    destination_port = packet[UDP].dport
-    timestamp = packet.time
-    total_length = len(packet)
-
-    with open("output.txt", "a") as file:
-        file.write(f"{{ source_ip: {source_ip}, dest_ip: {destination_ip}, source_port: {source_port}, dest_port: {destination_port}, protocol: UDP, timestamp: {timestamp}, total_length: {total_length}, data: {packet.payload.hex()} }}\n")
+    processing_packet(packet)
 
 def process_icmp_packet(packet):
     """
     This function as her name says, process ICMP packets.
     Overall, this function do as the function above but for ICMP protocol
     """
-    source_ip = packet[IP].src
-    destination_ip = packet[IP].dst
-    timestamp = packet.time
-    total_length = len(packet)
-
-    with open("output.txt", "a") as file:
-        file.write(f"{{ source_ip: {source_ip}, dest_ip: {destination_ip}, protocol: ICMP, timestamp: {timestamp}, total_length: {total_length}, data: {packet.payload.hex()} }}\n")
+    processing_packet(packet)
 
 def process_igmp_packet(packet):
     """
     This function as her name says, process IGMP packets.
     Overall, this function do as the function above but for IGMP protocol
     """
-    source_ip = packet[IP].src
-    destination_ip = packet[IP].dst
-    timestamp = packet.time
-    total_length = len(packet)
+    processing_packet(packet)
 
-    with open("output.txt", "a") as file:
-        file.write(
-            f"{{ source_ip: {source_ip}, dest_ip: {destination_ip}, protocol: IGMP, timestamp: {timestamp}, total_length: {total_length}, data: {packet.payload.hex()} }}\n")
+def processing_packet(packet_to_send):
 
-def process_raw_packet(packet):
-    """
-    handling packets of a protocol other than TCP, UDP, ICMP, or IGMP.
-    """
+    if IP in packet_to_send:
+        source_ip = packet_to_send[IP].src
+        destination_ip = packet_to_send[IP].dst
+        source_port = packet_to_send[TCP].sport
+        destination_port = packet_to_send[TCP].dport
+        timestamp = packet_to_send.time
+        total_length = len(packet_to_send)
 
+        if Raw in packet_to_send:
+            cache_flag = packet_to_send[Raw].load
+        else:
+            cache_flag = b''  # Set a default value or handle it according to your requirements
+
+        # Modify or replace with appropriate values for the following flags
+        steps_flag = ""
+        type_flag = ""
+        status_code = ""
+        cache_control = ""
+
+        if hasattr(packet_to_send.payload, 'hexdump'):  # Check if hexdump method is available
+            payload_hex = packet_to_send.payload.hexdump()  # Use hexdump instead of hex
+        else:
+            payload_hex = ''  # Set a default value or handle it according to your requirements
+        with open("SnifferFile.txt", "a") as file:
+            file.write(
+                f"{{ source_ip: {source_ip}, dest_ip: {destination_ip}, source_port: {source_port}, dest_port: {destination_port}"
+                f", timestamp: {timestamp}, total_length: {total_length}, cache_flag: {cache_flag.hex()}, steps_flag: {steps_flag},"
+                f" type_flag: {type_flag}, status_code: {status_code}, cache_control: {cache_control}, data: {payload_hex} }}\n")
 
 def process_packet(packet):
     """
@@ -81,8 +75,6 @@ def process_packet(packet):
         process_icmp_packet(packet)
     elif IGMP in packet:
         process_igmp_packet(packet)
-    else:
-        process_raw_packet(packet)
 
 
-sniff(filter="", prn=process_packet)
+sniff(filter="tcp", prn=process_packet)
